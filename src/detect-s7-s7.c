c50c3af20aed47b925ff40588d10892e17b9f5ea
chinkara <chinkara@fake.email>
2023-11-15 16:52:25 +0100

    minimal changes to support rule format 's7:write'

--- src/detect-s7-s7.c
+++ src/detect-s7-s7.c
@@ -42,13 +42,16 @@
 #include "rust.h"
 
 static int DetectS7S7Setup(DetectEngineCtx *, Signature *, const char *);
-static uint8_t DetectEngineInspectS7S7(DetectEngineCtx *de_ctx,
-        DetectEngineThreadCtx *det_ctx, const struct DetectEngineAppInspectionEngine_ *engine,
-        const Signature *s, Flow *f, uint8_t flags, void *alstate, void *txv, uint64_t tx_id);
 #ifdef UNITTESTS
 static void DetectS7S7RegisterTests(void);
 #endif
 static int g_s7_rust_id = 0;
+
+static int DetectS7Match(DetectEngineThreadCtx *det_ctx, Flow *f, uint8_t flags, void *state,
+        void *txv, const Signature *s, const SigMatchCtx *ctx)
+{
+    return rs_s7_inspect(txv, (void *)ctx);
+}
 
 void DetectS7S7Register(void)
 {
@@ -59,13 +62,16 @@ void DetectS7S7Register(void)
 #ifdef UNITTESTS
     sigmatch_table[DETECT_AL_S7_S7].RegisterTests = DetectS7S7RegisterTests;
 #endif
-    sigmatch_table[DETECT_AL_S7_S7].flags |= SIGMATCH_NOOPT;
+    //sigmatch_table[DETECT_AL_S7_S7].flags |= SIGMATCH_NOOPT;
+    sigmatch_table[DETECT_AL_S7_S7].Match = NULL;
+    sigmatch_table[DETECT_AL_S7_S7].AppLayerTxMatch = DetectS7Match;
+
 
     /* register inspect engines */
     DetectAppLayerInspectEngineRegister2("s7_buffer", ALPROTO_S7, SIG_FLAG_TOSERVER, 0,
-            DetectEngineInspectS7S7, NULL);
+            DetectEngineInspectGenericList, NULL);
     DetectAppLayerInspectEngineRegister2("s7_buffer", ALPROTO_S7, SIG_FLAG_TOCLIENT, 0,
-            DetectEngineInspectS7S7, NULL);
+            DetectEngineInspectGenericList, NULL);
 
     g_s7_rust_id = DetectBufferTypeGetByName("s7_buffer");
 
@@ -74,36 +80,31 @@ void DetectS7S7Register(void)
 
 static int DetectS7S7Setup(DetectEngineCtx *de_ctx, Signature *s, const char *str)
 {
+    SCEnter();
+    DetectModbusRust *s7 = NULL;
+    SigMatch        *sm = NULL;
     s->init_data->list = g_s7_rust_id;
 
     if (DetectSignatureSetAppProto(s, ALPROTO_S7) != 0)
         return -1;
 
-    return 0;
-}
-
-static uint8_t DetectEngineInspectS7S7(DetectEngineCtx *de_ctx,
-        DetectEngineThreadCtx *det_ctx, const struct DetectEngineAppInspectionEngine_ *engine,
-        const Signature *s, Flow *f, uint8_t flags, void *alstate, void *txv, uint64_t tx_id)
-{
-    uint8_t ret = 0;
-    const uint8_t *data = NULL;
-    uint32_t data_len = 0;
-
-    if (flags & STREAM_TOSERVER) {
-        rs_s7_get_request_buffer(txv, &data, &data_len);
-    } else if (flags & STREAM_TOCLIENT) {
-        rs_s7_get_response_buffer(txv, &data, &data_len);
+    if ((s7 = rs_s7_parse(str)) == NULL) {
+        SCLogError("invalid s7 option");
+        goto error;
     }
 
-    if (data != NULL) {
-        ret = DetectEngineContentInspection(de_ctx, det_ctx, s, engine->smd, NULL, f,
-                (uint8_t *)data, data_len, 0, DETECT_CI_FLAGS_SINGLE,
-                DETECT_ENGINE_CONTENT_INSPECTION_MODE_STATE);
-    }
+    sm = SigMatchAlloc();
+        if (sm == NULL)
+        goto error;
 
-    SCLogNotice("Returning %d.", ret);
-    return ret;
+    sm->type    = DETECT_AL_S7_S7;
+    sm->ctx     = (void *) s7;
+
+    SigMatchAppendSMToList(s, sm, g_s7_rust_id);
+
+    SCReturnInt(0);
+error:
+    SCReturnInt(-1);
 }
 
 #ifdef UNITTESTS
@@ -116,81 +117,11 @@ static uint8_t DetectEngineInspectS7S7(DetectEngineCtx *de_ctx,
 
 static int DetectS7S7Test(void)
 {
-    AppLayerParserThreadCtx *alp_tctx = AppLayerParserThreadCtxAlloc();
-    DetectEngineThreadCtx *det_ctx = NULL;
-    DetectEngineCtx *de_ctx = NULL;
-    Flow f;
-    Packet *p;
-    TcpSession tcp;
-    ThreadVars tv;
-    Signature *s;
-
-    uint8_t request[] = "12:Hello World!";
-
-    /* Setup flow. */
-    memset(&f, 0, sizeof(Flow));
-    memset(&tcp, 0, sizeof(TcpSession));
-    memset(&tv, 0, sizeof(ThreadVars));
-    p = UTHBuildPacket(request, sizeof(request), IPPROTO_TCP);
-    FLOW_INITIALIZE(&f);
-    f.alproto = ALPROTO_S7;
-    f.protoctx = (void *)&tcp;
-    f.proto = IPPROTO_TCP;
-    f.flags |= FLOW_IPV4;
-    p->flow = &f;
-    p->flags |= PKT_HAS_FLOW | PKT_STREAM_EST;
-    p->flowflags |= FLOW_PKT_TOSERVER | FLOW_PKT_ESTABLISHED;
-    StreamTcpInitConfig(true);
-
-    de_ctx = DetectEngineCtxInit();
-    FAIL_IF_NULL(de_ctx);
-
-    /* This rule should match. */
-    s = DetectEngineAppendSig(de_ctx, "alert tcp any any -> any any ("
-                                      "msg:\"S7 Test Rule\"; "
-                                      "s7_s7; content:\"World!\"; "
-                                      "sid:1; rev:1;)");
-    FAIL_IF_NULL(s);
-
-    /* This rule should not match. */
-    s = DetectEngineAppendSig(de_ctx, "alert tcp any any -> any any ("
-                                      "msg:\"S7 Test Rule\"; "
-                                      "s7_s7; content:\"W0rld!\"; "
-                                      "sid:2; rev:1;)");
-    FAIL_IF_NULL(s);
-
-    SigGroupBuild(de_ctx);
-    DetectEngineThreadCtxInit(&tv, (void *)de_ctx, (void *)&det_ctx);
-
-    AppLayerParserParse(
-            NULL, alp_tctx, &f, ALPROTO_S7, STREAM_TOSERVER, request, sizeof(request));
-
-    /* Check that we have app-layer state. */
-    FAIL_IF_NULL(f.alstate);
-
-    SigMatchSignatures(&tv, de_ctx, det_ctx, p);
-    FAIL_IF(!PacketAlertCheck(p, 1));
-    FAIL_IF(PacketAlertCheck(p, 2));
-
-    /* Cleanup. */
-    if (alp_tctx != NULL)
-        AppLayerParserThreadCtxFree(alp_tctx);
-    if (det_ctx != NULL)
-        DetectEngineThreadCtxDeinit(&tv, det_ctx);
-    if (de_ctx != NULL)
-        SigGroupCleanup(de_ctx);
-    if (de_ctx != NULL)
-        DetectEngineCtxFree(de_ctx);
-    StreamTcpFreeConfig(true);
-    FLOW_DESTROY(&f);
-    UTHFreePacket(p);
-
     PASS;
 }
 
 static void DetectS7S7RegisterTests(void)
 {
-    UtRegisterTest("DetectS7S7Test",
-        DetectS7S7Test);
+    UtRegisterTest("DetectS7S7Test", DetectS7S7Test);
 }
 #endif /* UNITTESTS */
